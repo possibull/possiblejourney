0bddb7b082b2cccaff5b0aec0c7c7f09461c3beb
Ted Possible <ted@mrpossible.com>
2025-07-22 16:04:50 -0700

    Initial Commit

---
+++ PossibleJourney/PossibleJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PossibleJourneyApp.swift
+//  PossibleJourney
+//
+//  Created by Ted Possible on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PossibleJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
