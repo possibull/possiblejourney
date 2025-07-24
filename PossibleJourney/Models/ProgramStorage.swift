59590b98b9f503fd01c4b060430476bc68d127e7
Ted Possible <ted@mrpossible.com>
2025-07-24 16:06:24 -0700

    Remove debug print statements from ProgramStorage (and none found in Program)

--- PossibleJourney/Models/ProgramStorage.swift
+++ PossibleJourney/Models/ProgramStorage.swift
@@ -4,27 +4,16 @@ struct ProgramStorage {
     private let key = "SavedProgram"
     
     func save(_ program: Program) {
-        print("DEBUG: Saving program: \(program)")
-        print("DEBUG: Task IDs being saved: \(program.tasks.map { $0.id.uuidString })")
         if let data = try? JSONEncoder().encode(program) {
             UserDefaults.standard.set(data, forKey: key)
-            print("DEBUG: Program data saved to UserDefaults")
-        } else {
-            print("DEBUG: Failed to encode program")
         }
     }
     
     func load() -> Program? {
-        print("DEBUG: ProgramStorage.load() called")
         guard let data = UserDefaults.standard.data(forKey: key) else {
-            print("DEBUG: No data found for key \(key)")
             return nil
         }
         let program = try? JSONDecoder().decode(Program.self, from: data)
-        print("DEBUG: Loaded program: \(String(describing: program))")
-        if let program = program {
-            print("DEBUG: Task IDs loaded: \(program.tasks.map { $0.id.uuidString })")
-        }
         return program
     }
     
