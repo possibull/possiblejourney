16e500a4a33d8b2b0ba7f501e4d83670e155ee74
Ted Possible <ted@mrpossible.com>
2025-07-24 16:05:12 -0700

    Add stubs for appDay(for:) and isDayMissed(for:completedTaskIDs:) to Program (TDD)

--- PossibleJourney/Models/Program.swift
+++ PossibleJourney/Models/Program.swift
@@ -6,4 +6,15 @@ struct Program: Codable {
     let numberOfDays: Int
     var tasks: [Task]
     var endOfDayTime: Date = Calendar.current.startOfDay(for: Date()) // Default 12:00AM
+}
+
+extension Program {
+    func appDay(for date: Date) -> Int {
+        // TODO: Implement logic
+        return -1 // placeholder to make test fail
+    }
+    func isDayMissed(for date: Date, completedTaskIDs: Set<UUID>) -> Bool {
+        // TODO: Implement logic
+        return false // placeholder to make test fail
+    }
 } 
