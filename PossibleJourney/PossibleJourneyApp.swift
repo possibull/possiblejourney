ec98df61165f3ba66f8e47e65183c3200d9a9808
Ted Possible <ted@mrpossible.com>
2025-08-02 04:00:32 -0700

    Change heart sequence back to emoji hearts - now shows purple heart, 3 yellow hearts, then 4 purple hearts using actual emojis

--- PossibleJourney/PossibleJourneyApp.swift
+++ PossibleJourney/PossibleJourneyApp.swift
@@ -26,7 +26,7 @@ struct BeaNumberSequenceView: View {
         ["x", "o", "o", "o", "x", "x", "x", "x"],
         ["X", "O", "O", "O", "X", "X", "X", "X"],
         [".", "-", "-", "-", ".", ".", "."],
-        ["ph", "yh", "yh", "yh", "ph", "ph", "ph", "ph"]
+        ["💜", "💛", "💛", "💛", "💜", "💜", "💜", "💜"]
     ]
     @State private var currentSequence: [String] = []
     private let displayDuration: TimeInterval = 0.8
